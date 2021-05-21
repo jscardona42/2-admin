efa7210b558451c2e53fbcd6f39cf2efb08143e0
Johan Cardona <jcardona@tiresia.co>
2021-05-21 09:10:07 -0500

    CME-20-2 Tokens encryption

--- src/admin/guard/authguard.guard.ts
+++ src/admin/guard/authguard.guard.ts
@@ -2,6 +2,7 @@ import { Injectable, ExecutionContext, UnauthorizedException } from "@nestjs/com
 import { GqlExecutionContext } from "@nestjs/graphql";
 import { AuthGuard } from "@nestjs/passport";
 import * as jwt from 'jsonwebtoken';
+import * as CryptoJS from 'crypto-js';
 
 @Injectable()
 export class GqlAuthGuard extends AuthGuard('jwt') {
@@ -17,11 +18,16 @@ export class GqlAuthGuard extends AuthGuard('jwt') {
         req = ctx.getContext().req;
 
         if (query === "getMethods") {
-
             req = ctx.getArgs().req;
 
+            if (req.headers.authorization_url === undefined) {
+                throw new UnauthorizedException("Unauthorized");
+            }
+
+            let referer = CryptoJS.AES.decrypt(req.headers.authorization_url, process.env.KEY_CRYPTO_ADMIN).toString(CryptoJS.enc.Utf8);
+
             try {
-                var url = jwt.verify(req.headers.authorization_url, process.env.JWT_SECRET_URL);
+                var url = jwt.verify(referer, process.env.JWT_SECRET_URL);
                 if (url === process.env.JWT_URL) {
                     return true;
                 }
@@ -30,14 +36,17 @@ export class GqlAuthGuard extends AuthGuard('jwt') {
             }
         }
 
-        const authorization = req.headers.authorization;
-        const referer = req.headers.authorization_url;
+        let authorization = req.headers.authorization;
+        let referer = req.headers.authorization_url;
 
         if ((referer === undefined) || (query !== "signInLogin" && query !== "logOutLogin" && authorization === undefined)) {
             throw new UnauthorizedException("Unauthorized");
         } else if (query === "signInLogin" || query === "logOutLogin") {
             return true;
         }
+
+        authorization = CryptoJS.AES.decrypt(authorization, process.env.KEY_CRYPTO).toString(CryptoJS.enc.Utf8);
+        referer = CryptoJS.AES.decrypt(referer, process.env.KEY_CRYPTO).toString(CryptoJS.enc.Utf8);
 
         try {
             jwt.verify(authorization.split(" ")[1], process.env.JWT_SECRET);
