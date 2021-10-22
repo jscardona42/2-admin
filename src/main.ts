ab89221e7ddf65053ca9ed08d86bcb0072ddca2e
jscardona42 <35873185+jscardona42@users.noreply.github.com>
2021-10-22 15:23:21 -0500

    CME-32-2 Se crea variable PORT en el .env

--- src/main.ts
+++ src/main.ts
@@ -9,6 +9,6 @@ async function bootstrap() {
   app.useGlobalGuards(new GqlAuthGuard());
   app.useGlobalPipes(new ValidationPipe());
   app.use(cookieParser());
-  await app.listen(3000);
+  await app.listen(process.env.PORT);
 }
 bootstrap();
