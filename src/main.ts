c8fbad5b47a3e0bbb87d4f80dbd4d0d20f644b6b
Johan Sebastian Cardona Gomez <jcardona@tiresia.co>
2021-12-16 14:49:57 -0500

    CME-169 Se activa Guard

--- src/main.ts
+++ src/main.ts
@@ -6,7 +6,7 @@ var cookieParser = require('cookie-parser');
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  // app.useGlobalGuards(new GqlAuthGuard());
+  app.useGlobalGuards(new GqlAuthGuard());
   app.useGlobalPipes(new ValidationPipe());
   app.use(cookieParser());
   await app.listen(process.env.PORT || 3000);
